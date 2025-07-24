e0a3ea2e463474338f686948e3937fa992d76789
Lucas Silva <lucas.silva182@outlook.com>
2025-07-24 11:57:22 -0300

    feat: add footer component and config Layout component css

---
+++ src/components/ui/Footer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const year = ref(new Date().getFullYear());
+</script>
+
+<template>
+  <header
+    class="flex justify-center sticky z-[100] top-0 bg-background backdrop-blur-lg border-t border-border w-full"
+  >
+    <div class="container flex h-[70px] items-center justify-between">
+      <p>{{ year }} © Todos os direitos reservados</p>
+    </div>
+  </header>
+</template>
--- src/pages/Layout.vue
+++ src/pages/Layout.vue
@@ -1,10 +1,14 @@
 <script setup>
 import Header from '@/components/ui/Header.vue';
+import Footer from '../components/ui/Footer.vue';
 </script>
 
 <template>
-  <div class="flex justify-center items-center flex-col">
+  <div class="flex flex-col min-h-screen">
     <Header />
-    <router-view />
+    <main class="flex-grow">
+      <router-view class="min-h-full" />
+    </main>
+    <Footer />
   </div>
 </template>
